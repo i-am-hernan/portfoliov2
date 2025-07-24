5936a6a88d0be90e82672fd9e9f381d3b11c378a
Hernan <hschalco@gmail.com>
2025-07-23 21:27:57 -0700

    ugh

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -5,30 +5,30 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 function SpinningBox() {
-  const meshRef = useRef<THREE.Mesh>(null!);
-  useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta * 0.5;
-    meshRef.current.rotation.y += delta * 0.7;
-  });
-  return (
-    <mesh ref={meshRef} position={[0, 0, 0]} castShadow>
-      <sphereGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="#6366f1" />
-    </mesh>
-  );
+	const meshRef = useRef<THREE.Mesh>(null!);
+	useFrame((state, delta) => {
+		meshRef.current.rotation.x += delta * 0.5;
+		meshRef.current.rotation.y += delta * 0.5;
+	});
+	return (
+		<mesh ref={meshRef} position={[0, 0, 0]} castShadow>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color="#6366f1" />
+		</mesh>
+	);
 }
 
 const Scene = () => {
-  return (
-    <div style={{ width: "100%", height: "400px" }}>
-      <Canvas camera={{ position: [2, 2, 2], fov: 50 }} shadows>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
-        <SpinningBox />
-        <OrbitControls enablePan={false} />
-      </Canvas>
-    </div>
-  );
+	return (
+		<div style={{ width: "100%", height: "300px", backgroundColor: "black" }}>
+			<Canvas camera={{ position: [2, 2, 2], fov: 50 }} shadows>
+				<ambientLight intensity={0.5} />
+				<directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+				<SpinningBox />
+				<OrbitControls enablePan={false} />
+			</Canvas>
+		</div>
+	);
 };
 
 export default Scene;
